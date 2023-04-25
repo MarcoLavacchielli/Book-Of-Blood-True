b9c21c3bff532b8507604c1431a26d94595dc931
facutise <facundo.tisera123@gmail.com>
2023-04-25 20:55:33 -0300

    nuevo script para estadisticas

---
+++ Assets/Scripts/StadisticPlayer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StadisticPlayer : MonoBehaviour
+{
+    public int health = 30;
+    public int vigor = 40;
+}
