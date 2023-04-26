9dedf9289921846bef85ec4b18e073561bc76bff
Marco <marco.lavacchielli@davinci.edu.ar>
2023-04-26 18:18:23 -0300

    Menu principal y sus funciones

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,34 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject MainMenu;
     public GameObject MenuTutorial;
     public GameObject MenuCredits;
+    public string scenename;
 
-
-        private void Awake()
+    void loadscene()
     {
-        ActivarMainMenu();
-    }
-    public void ActivarMainMenu()
-    {
-        MainMenu.SetActive(true);
-        MenuCredits.SetActive(false);
-        MenuTutorial.SetActive(false);
-    }
-    public void ActivarTutorial()
-    {
-        MainMenu.SetActive(false);
-        MenuCredits.SetActive(false);
-        MenuTutorial.SetActive(true);
-    }
-    public void ActivarCredits()
-    {
-        MainMenu.SetActive(false);
-        MenuCredits.SetActive(true);
-        MenuTutorial.SetActive(false);
+        //SceneManager.LoadScene();
     }
 }
